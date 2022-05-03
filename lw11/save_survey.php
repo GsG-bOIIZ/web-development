817a6e8f592f8889acff93951361a1c679d74b5c
GsG-bOIIZ <71519799+GsG-bOIIZ@users.noreply.github.com>
2022-05-03 15:08:33 +0300

    Add files via upload

---
+++ lw11/save_survey.php
@@ -0,0 +1,7 @@
+<?php
+require_once('./src/common.inc.php'); 
+$requestSurveyLoader = new RequestSurveyLoader();
+$surveyFileStorage = new SurveyFileStorage();
+
+$survey = $requestSurveyLoader->createSurveyInfo()
+SurveyFileStorage::saveSurveyToFile($survey);
